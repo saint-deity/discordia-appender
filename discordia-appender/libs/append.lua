079c0f210547934c548cdd04a46ddf699e4a3daa
Deity <59446525+saint-deity@users.noreply.github.com>
2022-09-29 01:17:52 +1000

    Reading DATA fix.
    Fix the issue caused when trying to read data and returning nil.

--- discordia-appender/libs/append.lua
+++ discordia-appender/libs/append.lua
@@ -22,43 +22,53 @@ local append = { }
 
 function append:sort(data)
     prettyPrint.loadColors(256)
+    if data[1] then
+        print(data[1]["content"])
+    end
     if type(data) == "table" then
+        for i,v in pairs(data) do
+            print(i)
+        end
+
         local sorted = { }
-        
+        local sortedi = { }
+
         for index, value in pairs(data) do
-            if not data[index]["username"] then return print(color("err", "[Appender]").." | ERR | No username provided") end
-            if not data[index]["avatar_url"] then return print(color("err", "[Appender]").." | ERR | No avatar URL provided") end
-            if not data[index]["content"] then return print(color("err", "[Appender]").." | ERR | No content provided") end
-            if not data[index]["timestamp"] then return print(color("err", "[Appender]").." | ERR | No timestamp provided") end
+            if index == nil then return print(color("err", "[Appender]").." | ERR | Unexpected Error") end
+            if not value['username'] then return print(color("err", "[Appender]").." | ERR | No username provided") end
+            if not value['avatar_url'] then return print(color("err", "[Appender]").." | ERR | No avatar URL provided") end
+            if not value['content'] then return print(color("err", "[Appender]").." | ERR | No content provided") end
+            if not value['timestamp'] then return print(color("err", "[Appender]").." | ERR | No timestamp provided") end
 
             local res = [[
             <div class="card">
                 <div style="display: inline-block; vertical-align: top;">
-                    <img src="]]..data[index]["avatar_url"]..[[" alt="user icon" class="avatar" width="40px" height="40px">   
+                    <img src="]]..value['avatar_url']..[[" alt="user icon" class="avatar" width="40px" height="40px">   
                 </div>
         
                 <div style="color: white; position: relative; top: 0px; display: inline-block;">
                     <span style="font-size: small;">
-                        ]]..data[index]["username"]..[[
+                        ]]..value['username']..[[
                         <span style="opacity: 20%;">
-                            ]]..data[index]["timestamp"]..[[        
+                            ]]..value['timestamp']..[[        
                         </span>
                     </span>
 
                     <br>
 
                     <span style="font-size: medium;">
-                        ]]..data[index]["content"]..[[
+                        ]]..value['content']..[[
 
                     </span>
                 </div>
             </div>
 ]]
-
+            print(res)
             insert(sorted, #sorted+1, res)
         end
 
-        local ret = [[
+        if sorted[1] then
+            local ret = [[
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -86,12 +96,64 @@ function append:sort(data)
 
     <body>
         <div class="cards">
-]]..concat(sorted, "\n")..[[
+]]..table.concat(sorted)..[[
     </div>
 </html>
 ]]
-
-        return ret
+return ret
+        else
+            print(color("err", "[Appender]").." | ERR | Unexpected Error")
+            return [[
+                <!DOCTYPE html>
+                <html lang="en">
+                    <head>
+                        <meta charset="UTF-8">
+                        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+                        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                
+                        <title>Appended Transcript</title>
+                
+                        <style>
+                            body {
+                                background-color: #2c2f33;
+                                font-family: sans-serif;
+                            }
+                
+                            img {
+                                border-radius: 50%;
+                            }
+                
+                            .card {
+                                margin-top: 10px;
+                            }
+                        </style>
+                    </head>
+                
+                    <body>
+                        <div class="cards">
+                            <div class="card">
+                                <div style="display: inline-block; vertical-align: top;">
+                                    <img src="https://cdn.discordapp.com/avatars/1023482322890281042/4fb15c961276e9c8e5b35603d266c756.webp?size=240" alt="user icon" class="avatar" width="40px" height="40px">   
+                                </div>
+                        
+                                <div style="color: white; position: relative; top: 0px; display: inline-block;">
+                                    <span style="font-size: small;">
+                                        Ken and Richie#8601                        <span style="opacity: 20%;">
+                                            01/01/1970         
+                                        </span>
+                                    </span>
+                
+                                    <br>
+                
+                                    <span style="font-size: medium;">
+                                        Hello, world!
+                                    </span>
+                                </div>
+                            </div>
+                        </div>
+                    </body>
+                </html>]]
+        end
     else
         return print(color("err", "[Appender]").." | ERR | type \"table\" expected (got "..type(data)..")")
     end
@@ -108,6 +170,7 @@ function append:write(file, content)
     local file = open(file, "w")
     file:write(content)
     file:close()
+    
 end
 
 return append
