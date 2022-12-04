4615968fe07d40aa091a848e1eaa3e9ec6c6e1c6
Deity <59446525+saint-deity@users.noreply.github.com>
2022-12-04 16:12:21 +1000

    added readJSON handlers
    Was having trouble with the readJSON method return `null` / `nil`, so I implemented some handlers to avoid it from always breaking for no reason.
    The cause of it returning `null` / `nil` is remains a mystery, but it now reads the file correctly without invalid option type to :read #1
    
    Creating a new branch until I can confirm that all methods work properly.

--- discordia-appender/libs/files.lua
+++ discordia-appender/libs/files.lua
@@ -29,10 +29,22 @@ end
 function files:readJSON(file)
     local ret
 
+    if not file then
+        return (print(color("err", "[APPENDER/JSON]") .. " | ERR | Invalid argument #1 (file), string not provided."))
+    end
+
+    if type(file) ~= "string" then
+        return  (print(color("err", "[APPENDER/JSON]") .. " | ERR | Invalid argument #1 (file), expected string [got " .. type(file) .. "]"))
+    end
+
     if open(file, "r") then
-        local file = open(file, "r")
-        ret = parse(file:read(file))
+        print(file)
+        local file = assert(open(file, "r"))
+        ret = parse(file:read("*all"))
         file:close()
+        if ret == "" or not ret then
+            return (print(color("err", "[APPENDER/JSON]") .. " | ERR | Encountered error while decoding JSON to lua table [returned nil]"))
+        end
     else
         return (print(color("err", "[APPENDER/JSON]").." | ERR | File does not exist"))
     end
